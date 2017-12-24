292b754aaff95746f945ff08088561c46ed3e1b9
Nathan Blair <nathan.t.blair@outlook.com>
2017-12-24 06:35:34 -0500

    Reorganization of web app

--- server.js
+++ server.js
@@ -1,38 +1,22 @@
 const port = 8080
 const http = require('http')
-const url = require('url')
-const fs = require('fs')
+const wow = require('./wow/wow')
 
-function ServerAction(request, response) {
-	var uri = process.cwd() + request.url
-	url_parts = url.parse(request.url, true)
+function Router(request, response) {
+	var host = request.headers.host
+	var url = request.url
+	if (url.endsWith('/')) {
+		url += 'index.html'
+	}
 
-	if (uri.endsWith('/create')) {
-		// CREATE TODO
-	} else if (uri.endsWith('/edit')) {
-		// EDIT TODO
-	} else if (uri.endsWith('/delete')) {
-		// DELETE TODO
-	} else {
-		if (uri.endsWith('/')) {
-			uri += 'index.html'
-		}
-		console.log('Request for: ' + uri + ' from ' + request.connection.remoteAddress)
-		fs.readFile(uri, function(error, fileContents) {
-			if (error) {
-				console.log('Replied: 404\'d')
-				response.writeHead(404)
-				response.write(resp)
-			} else {
-				response.writeHead(200)
-				response.write(fileContents)
-				console.log('Replied with ' + uri)
-			}
-			response.end()
-		})
+	console.log('Request at {' + host + '} for {' + url + '} from {' + request.connection.remoteAddress + '}')
+	if (host.includes('wow')) {
+		wow.Controller(host, url, response)
 	}
 }
 
-http.createServer(ServerAction).listen(port)
+var server = http.createServer(Router)
+
+server.listen(port)
 
 console.log('Server listening on port: ' + port)
